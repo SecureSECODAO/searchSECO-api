3f4d96a3c358cf0354c5b5e095f850c2b0883a4e
Roderick <r.ras2@students.uu.nl>
2023-05-15 22:01:27 +0200

    refactor: nodegit instead of simple-git

--- src/searchseco/spider/Spider.ts
+++ src/searchseco/spider/Spider.ts
@@ -1,4 +1,4 @@
-import { Clone, Repository } from 'nodegit';
+import { Clone, Repository, Diff, ConvenientPatch, Blame } from 'nodegit';
 import * as fs from 'fs';
 import * as path from 'path';
 const glob = require('glob');
@@ -48,71 +48,71 @@ export default class Spider {
         }
     }
 
-    // /**
-    //  * Updates project from one version to another, keeping track of unchanged files.
-    //  * Versions should be in chronological order. Deletes unchanged files from local project.
-    //  *
-    //  * @param filePath Local path where project is stored.
-    //  * @param prevTag Name of current version.
-    //  * @param newTag Name of version to update to.
-    //  * @returns Unchanged files between versions.
-    //  */
-    // async updateRepo(filePath: string, prevTag: string, newTag: string, prevUnchangedFiles: string[]): Promise<string[]> {
-    //     if (!this.repo) {
-    //         throw new Error('Repository not initialized.');
-    //     }
+    /**
+     * Updates project from one version to another, keeping track of unchanged files.
+     * Versions should be in chronological order. Deletes unchanged files from local project.
+     *
+     * @param filePath Local path where project is stored.
+     * @param prevTag Name of current version.
+     * @param newTag Name of version to update to.
+     * @returns Unchanged files between versions.
+     */
+    async updateRepo(filePath: string, prevTag: string, newTag: string, prevUnchangedFiles: string[]): Promise<string[]> {
+        if (!this.repo) {
+            throw new Error('Repository not initialized.');
+        }
 
-    //     let changedFiles: string[] = [];
+        let changedFiles: string[] = [];
 
-    //     if (prevTag) {
-    //         const oldCommit = await this.repo.getReferenceCommit(prevTag);
-    //         const newCommit = await this.repo.getReferenceCommit(newTag);
-    //         const diffList = await nodegit.Diff.treeToTree(this.repo, await oldCommit.getTree(), await newCommit.getTree(), null);
-    //         const patches = await diffList.patches();
-    //         await Promise.all(patches.map(async (patch: nodegit.ConvenientPatch) => {
-    //             changedFiles.push(patch.newFile().path());
-    //         }));
-    //     }
+        if (prevTag) {
+            const oldCommit = await this.repo.getReferenceCommit(prevTag);
+            const newCommit = await this.repo.getReferenceCommit(newTag);
+            const diffList = await Diff.treeToTree(this.repo, await oldCommit.getTree(), await newCommit.getTree(), null);
+            const patches = await diffList.patches();
+            await Promise.all(patches.map(async (patch: ConvenientPatch) => {
+                changedFiles.push(patch.newFile().path());
+            }));
+        }
 
-    //     await this.checkoutBranch(newTag);
+        await this.checkoutBranch(newTag);
 
-    //     const allFiles: string[] = await new Promise((resolve, reject) => {
-    //         glob("**/*", { cwd: filePath, nodir: true, ignore: '**/.git/**' }, (err: Error | null, matches: string[]) => {
-    //             if (err) reject(err);
-    //             else resolve(matches);
-    //         });
-    //     });
+        const allFiles: string[] = await new Promise((resolve, reject) => {
+            glob("**/*", { cwd: filePath, nodir: true, ignore: '**/.git/**' }, (err: Error | null, matches: string[]) => {
+                if (err) reject(err);
+                else resolve(matches);
+            });
+        });
 
-    //     const unchangedFiles = allFiles.filter(file => !changedFiles.includes(file));
+        const unchangedFiles = allFiles.filter(file => !changedFiles.includes(file));
 
-    //     const removedFiles: string[] = [];
-    //     for (const file of unchangedFiles) {
-    //         if (prevUnchangedFiles.includes(file)) {
-    //             await fs.promises.unlink(path.join(filePath, file));
-    //             removedFiles.push(file);
-    //         }
-    //     }
+        const removedFiles: string[] = [];
+        for (const file of unchangedFiles) {
+            if (prevUnchangedFiles.includes(file)) {
+                await fs.promises.unlink(path.join(filePath, file));
+                removedFiles.push(file);
+            }
+        }
 
-    //     return removedFiles;
-    // }
+        return removedFiles;
+    }
 
-    // /**
-    //  * Switches local project to different version.
-    //  *
-    //  * @param tag Name of the version to update to.
-    //  * @param filePath Local path where project is stored.
-    // */
-    // async switchVersion(tag: string): Promise<void> {
-    //     try {
-    //         if (!this.repo) {
-    //             throw new Error('Repository not initialized.');
-    //         }
-    //         await this.checkoutBranch(tag);
-    //     } catch (error) {
-    //         console.error(`Failed to switch to version ${tag}: ${error}`);
-    //         throw error;
-    //     }
-    // }
+    /**
+     * Switches local project to different version.
+     *
+     * @param tag Name of the version to update to.
+     * @param filePath Local path where project is stored.
+    */
+    async switchVersion(tag: string): Promise<void> {
+        try {
+            if (!this.repo) {
+                throw new Error('Repository not initialized.');
+            }
+            await this.checkoutBranch(tag);
+        } catch (error) {
+            console.error(`Failed to switch to version ${tag}: ${error}`);
+            throw error;
+        }
+    }
 
     private async checkoutBranch(branchName: string): Promise<void> {
         if (!this.repo) {
@@ -122,114 +122,114 @@ export default class Spider {
         await this.repo.checkoutRef(reference); 
     }
 
-    // /**
-    // * Trims the local files to only keep the specified ones.
-    // *
-    // * @param filePath The path into which the project was cloned.
-    // * @param lines The files to keep.
-    // */
-    // async trimFiles(filePath: string, lines: Map<string, number[]>): Promise<void> {
-    //     try {
-    //         const files: string[] = await new Promise((resolve, reject) => {
-    //             glob("**/*", { cwd: filePath, nodir: true, ignore: '**/.git/**' }, (err: Error | null, matches: string[]) => {
-    //                 if (err) reject(err);
-    //                 else resolve(matches);
-    //             });
-    //         });
+    /**
+    * Trims the local files to only keep the specified ones.
+    *
+    * @param filePath The path into which the project was cloned.
+    * @param lines The files to keep.
+    */
+    async trimFiles(filePath: string, lines: Map<string, number[]>): Promise<void> {
+        try {
+            const files: string[] = await new Promise((resolve, reject) => {
+                glob("**/*", { cwd: filePath, nodir: true, ignore: '**/.git/**' }, (err: Error | null, matches: string[]) => {
+                    if (err) reject(err);
+                    else resolve(matches);
+                });
+            });
 
-    //         for (const file of files) {
-    //             const fileString: string = path.normalize(file).replace(filePath, '');
-    //             if (!lines.has(fileString)) {
-    //                 await fs.promises.unlink(path.join(filePath, fileString));
-    //             }
-    //         }
-    //     } catch (error) {
-    //         console.error(`Failed to trim files in ${filePath}: ${error}`);
-    //     }
-    // }
+            for (const file of files) {
+                const fileString: string = path.normalize(file).replace(filePath, '');
+                if (!lines.has(fileString)) {
+                    await fs.promises.unlink(path.join(filePath, fileString));
+                }
+            }
+        } catch (error) {
+            console.error(`Failed to trim files in ${filePath}: ${error}`);
+        }
+    }
 
-    // /**
-    // * Retrieves and returns author information for all files in the given repository.
-    // *
-    // * The method opens the repository, retrieves all files, and for each file, 
-    // * retrieves all blocks of code along with the commit data for each block.
-    // * The commit data includes information about the author and committer, 
-    // * as well as the summary and hash of the previous commit.
-    // *
-    // * @param repoPath The path to the repository.
-    // * @returns A promise that resolves to a Map. 
-    // *          Each key in the Map is a filename and each value is an array of code blocks. 
-    // *          Each code block includes the line number, number of lines, and commit data for the block.
-    // * @throws Error if the repository is not initialized.
-    // */
-    // async downloadAuthor(repoPath: string): Promise<AuthorData> {
-    //     try {
-    //         if (!this.repo) {
-    //             throw new Error('Repository not initialized.');
-    //         }
-    //         const repo = await nodegit.Repository.open(repoPath);
-    //         const files = await this.getAllFiles(repoPath);
-    //         const authorData: AuthorData = new Map();
+    /**
+    * Retrieves and returns author information for all files in the given repository.
+    *
+    * The method opens the repository, retrieves all files, and for each file, 
+    * retrieves all blocks of code along with the commit data for each block.
+    * The commit data includes information about the author and committer, 
+    * as well as the summary and hash of the previous commit.
+    *
+    * @param repoPath The path to the repository.
+    * @returns A promise that resolves to a Map. 
+    *          Each key in the Map is a filename and each value is an array of code blocks. 
+    *          Each code block includes the line number, number of lines, and commit data for the block.
+    * @throws Error if the repository is not initialized.
+    */
+    async downloadAuthor(repoPath: string): Promise<AuthorData> {
+        try {
+            if (!this.repo) {
+                throw new Error('Repository not initialized.');
+            }
+            const repo = await Repository.open(repoPath);
+            const files = await this.getAllFiles(repoPath);
+            const authorData: AuthorData = new Map();
     
-    //         // Process each file
-    //         for (const file of files) {
-    //             const codeBlocks = await this.processFile(repo, file);
-    //             authorData.set(file, codeBlocks);
-    //         }
+            // Process each file
+            for (const file of files) {
+                const codeBlocks = await this.processFile(repo, file);
+                authorData.set(file, codeBlocks);
+            }
     
-    //         return authorData;
-    //     } catch (error) {
-    //         console.error('An error occurred:', error);
-    //         throw error;
-    //     }
-    // }
+            return authorData;
+        } catch (error) {
+            console.error('An error occurred:', error);
+            throw error;
+        }
+    }
     
-    // private async processFile(repo: nodegit.Repository, file: string): Promise<CodeBlock[]> {
-    //     if (!this.repo) {
-    //         throw new Error('Repository not initialized.');
-    //     }
-    //     const blame = await nodegit.Blame.file(repo, file);
-    //     const numHunks = blame.getHunkCount();
+    private async processFile(repo: Repository, file: string): Promise<CodeBlock[]> {
+        if (!this.repo) {
+            throw new Error('Repository not initialized.');
+        }
+        const blame = await Blame.file(repo, file);
+        const numHunks = blame.getHunkCount();
     
-    //     const codeBlocks: CodeBlock[] = [];
+        const codeBlocks: CodeBlock[] = [];
     
-    //     for (let i = 0; i < numHunks; i++) {
-    //         const hunk = blame.getHunkByIndex(i);
-    //         const commit = await repo.getCommit(hunk.finalCommitId());
+        for (let i = 0; i < numHunks; i++) {
+            const hunk = blame.getHunkByIndex(i);
+            const commit = await repo.getCommit(hunk.finalCommitId());
     
-    //         const author = commit.author();
-    //         const committer = commit.committer();
+            const author = commit.author();
+            const committer = commit.committer();
             
-    //         const commitData: CommitData = {
-    //             author: author.name(),
-    //             authorMail: author.email(),
-    //             authorTime: author.when().toString(),
-    //             authorTz: author.when().offset.toString(),
-    //             committer: committer.name(),
-    //             committerMail: committer.email(),
-    //             committerTime: committer.when().toString(),
-    //             committerTz: committer.when().offset.toString(),
-    //             summary: commit.summary(),
-    //             previousHash: hunk.origCommitId().tostrS(),
-    //             fileName: hunk.origPath(),
-    //         };
+            const commitData: CommitData = {
+                author: author.name(),
+                authorMail: author.email(),
+                authorTime: author.when().toString(),
+                authorTz: author.when().offset.toString(),
+                committer: committer.name(),
+                committerMail: committer.email(),
+                committerTime: committer.when().toString(),
+                committerTz: committer.when().offset.toString(),
+                summary: commit.summary(),
+                previousHash: hunk.origCommitId().tostrS(),
+                fileName: hunk.origPath(),
+            };
     
-    //         codeBlocks.push({
-    //             line: hunk.finalStartLineNumber(),
-    //             numLines: hunk.linesInHunk(),
-    //             commit: commitData,
-    //         });
-    //     }
+            codeBlocks.push({
+                line: hunk.finalStartLineNumber(),
+                numLines: hunk.linesInHunk(),
+                commit: commitData,
+            });
+        }
     
-    //     return codeBlocks;
-    // }
+        return codeBlocks;
+    }
 
-    // private async getAllFiles(repoPath: string): Promise<string[]> {
-    //     const allFiles = await fs.promises.readdir(repoPath, { withFileTypes: true });
-    //     const fileNames = allFiles
-    //         .filter(dirent => dirent.isFile() && !/^.git/.test(dirent.name))
-    //         .map(dirent => dirent.name);
+    private async getAllFiles(repoPath: string): Promise<string[]> {
+        const allFiles = await fs.promises.readdir(repoPath, { withFileTypes: true });
+        const fileNames = allFiles
+            .filter(dirent => dirent.isFile() && !/^.git/.test(dirent.name))
+            .map(dirent => dirent.name);
 
-    //     return fileNames;
-    // }
+        return fileNames;
+    }
 }
